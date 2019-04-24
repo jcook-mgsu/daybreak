14bb76dbd82d864f9a85378eeaf6a7e31cb89835
jcook-ccc <jordan.cook@cccga.com>
2019-04-24 16:41:34 -0400

    added links to home page

--- application/views/home.php
+++ application/views/home.php
@@ -1,1 +1,30 @@
-hello world
+<div class="row">
+  <div class="col xl12">
+    <h2>What would you like do?</h2>
+  </div>
+</div>
+<div class="row">
+  <div class="col xl6">
+    <div class="card-panel blue lighten-2">
+      <h4 class="white-text">Participant Managment</h4>
+      <hr>
+      <p><a class="waves-effect waves-light btn red lighten-2" href="<?php echo base_url(); ?>participants/index">View Participants</a>&nbsp;<a class="waves-effect waves-light btn red lighten-1" href="#">New Participant</a></p>
+    </div>
+  </div>
+  <div class="col xl6">
+    <div class="card-panel red lighten-2">
+      <h4 class="white-text">Activity Managment</h4>
+      <hr>
+      <p><a class="waves-effect waves-light btn blue lighten-2" href="<?php echo base_url(); ?>participants/activity_log">Activity Log</a>&nbsp;<a class="waves-effect waves-light btn blue lighten-1" href="#">New Activity Type</a>&nbsp;<a class="waves-effect waves-light btn blue" href="#">Log an Activity</a></p>
+    </div>
+  </div>
+</div>
+<div class="row">
+  <div class="col xl6">
+    <div class="card-panel green lighten-2">
+      <h4 class="white-text">Account Managment</h4>
+      <hr>
+      <p><a class="waves-effect waves-light btn teal lighten-2" href="<?php echo base_url(); ?>auth/create_user">Create New User</a>&nbsp;<a class="waves-effect waves-light btn teal lighten-1" href="<?php echo base_url(); ?>auth/edit_user">Account</a></p>
+    </div>
+  </div>
+</div>
